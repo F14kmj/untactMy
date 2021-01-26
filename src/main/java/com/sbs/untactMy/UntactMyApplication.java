1512a5e2d07eb92acbda0c70fdc90896454760e4
f14kmj <f14kmj@naver.com>
2021-01-27 00:01:48 +0900

    First commit

---
+++ src/main/java/com/sbs/untactMy/UntactMyApplication.java
@@ -0,0 +1,13 @@
+package com.sbs.untactMy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntactMyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UntactMyApplication.class, args);
+	}
+
+}
